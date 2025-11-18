96ba01cfdd1fde748c2e684b272aeb62f1440fd1
Leonardo Santos <115591322+leosantos86@users.noreply.github.com>
2025-11-18 08:43:56 -0300

    Delete GodotWebSocketMultiplayer directory

--- GodotWebSocketMultiplayer/demo/cenas/botoes.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-func _ready():
-	# Esconde a interface de botões se não estiver no celular
-	# (útil caso queira mostrar só no mobile)
-	if not OS.has_feature("mobile"):
-		hide()
